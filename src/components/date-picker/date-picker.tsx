e1e1e7d03b0f51b531dc272e2aa5c68169544562
elijah688 <elijah.iliev.95@gmail.com>
2019-12-22 19:46:59 +0000

    build(date picker): started a new stencil project and added the date-picker component

---
+++ src/components/date-picker/date-picker.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from "@stencil/core";
+
+@Component({
+    tag:'elijah-date-picker',
+    styleUrl: 'date-picker.css',
+    shadow:true
+})
+export class DatePicker{
+    
+    render(){
+        return (<p>DATEPICKER WORKING</p>)
+    }
+}
